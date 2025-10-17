0695a1e1fecdace8a3f9ef42810ea54dde10bbfd
naka6ryo <naka6ryo@gmail.com>
2025-10-18 01:37:21 +0900

    CHARGE閾値更新

--- hand.js
+++ hand.js
@@ -30,11 +30,11 @@ const CFG = {
   },
   charge: {
     // PIP 関節の角度しきい値 (rad)。angleBetween(PIP->MCP, PIP->DIP) がこの値未満なら曲がっていると判定
-    angleThresholdRad: 1,
+    angleThresholdRad: 1.5,
     // CHARGE を開始するまでのホールド時間（秒）
     holdSec: 0.1,
     // MCP（第1関節）の角度もしきい値として考慮する（angle at MCP between wrist->MCP and PIP->MCP）
-    mcpAngleThresholdRad: 1,
+    mcpAngleThresholdRad: 1.5,
   },
 };
 
